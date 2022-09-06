ba5ed5655f9da675ce2d3ae8b37b4874b9b7e7df
Freek van de Ven <freek@freekvandeven.nl>
2022-09-06 11:24:35 +0200

    feat: working image progression widget

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,14 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:image_progression/image_progression.dart';
 
 void main() {
   runApp(const MaterialApp(home: ImageProgressionDemo()));
 }
 
-class ImageProgressionDemo extends StatelessWidget {
+class ImageProgressionDemo extends StatefulWidget {
   const ImageProgressionDemo({Key? key}) : super(key: key);
 
   @override
+  State<ImageProgressionDemo> createState() => _ImageProgressionDemoState();
+}
+
+class _ImageProgressionDemoState extends State<ImageProgressionDemo> {
+  double _progress = 0.0;
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Text('ImageProgressionDemo'));
+    return Scaffold(
+      body: Center(
+        child: Row(
+          children: [
+            Align(
+              alignment: Alignment.topLeft,
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.7,
+                height: MediaQuery.of(context).size.height * 0.7,
+                child: ImageProgression(
+                  progress: _progress,
+                  images: [
+                    const ImageProgress(
+                      image: AssetImage('assets/container_leeg.png'),
+                    ),
+                    const ImageProgress(
+                      image: AssetImage('assets/container_vol.png'),
+                    ),
+                  ],
+                  stops: [0.0, 1.0],
+                ),
+              ),
+            ),
+
+            // vertical slider to change the progress
+            Padding(
+              padding: EdgeInsets.symmetric(
+                vertical: MediaQuery.of(context).size.height * 0.2,
+              ),
+              child: RotatedBox(
+                quarterTurns: 3,
+                child: Slider(
+                  thumbColor: const Color(
+                    0xFFBBD10D,
+                  ),
+                  activeColor: const Color(
+                    0xFFBBD10D,
+                  ),
+                  inactiveColor: const Color(0xFF9C9C9B),
+                  value: _progress,
+                  onChanged: (value) {
+                    setState(() {
+                      _progress = value;
+                    });
+                  },
+                  min: 0.0,
+                  max: 1.0,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/image_progression.dart
+++ lib/image_progression.dart
@@ -1,1 +1,4 @@
 library image_progression;
+
+export 'package:image_progression/src/image_progression.dart';
+export 'package:image_progression/src/models/progress_image.dart';
